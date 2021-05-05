c4ea3ea696301ca1bbecc34c4527bca42ba00b74
Scoutacus <vanguardacus@gmail.com>
2021-05-05 18:52:52 -0400

    updated index.html to include onclick events that point to functions rock(), paper(), and scissors(). Also cleaned up singleRound() so that it explicitly handles RPS logic and returns score

--- newjava.js
+++ newjava.js
@@ -1,19 +1,30 @@
+//Uses Math.Random to randomly pick one of the 3 objects in the array as it's value to be passed on too singleRound()
 function computerPlay(){
     var items= ["rock", "paper", "scissors"];
     var index = Math.floor(Math.random() * items.length);
     return items[index];
 }
 
+//functions that call singleRound() on button click
+const rock = function(){
+    singleRound("rock", computerSelection= computerPlay());
+}
+const paper = function(){
+    singleRound("paper", computerSelection= computerPlay());
+}
+const scissors = function(){
+    singleRound("scissors", computerSelection= computerPlay());
+}
+
+//function that runs a single round of RPS
 function singleRound(playerSelection, computerSelection){
-    const buttons = document.querySelector('button');
-    buttons.forEach((buttons) => {
-        buttons.addEventListener('click', () =>{
+    let playerWin
+    let playerScore = 0;
+    let computerWin
+    let computerScore = 0;
 
-        })
-    });
 
-    computerSelection = computerPlay();
-
+    //RPS logic
     if (playerSelection == "rock" && computerSelection == "scissors"){
         console.log("Computer chose : " + computerSelection);
         console.log(playerWin = "Congrats, you win!");
@@ -27,4 +38,13 @@ function singleRound(playerSelection, computerSelection){
         console.log("Computer chose : " + computerSelection);
         console.log("Draw!")
     } else(console.log("Computer chose : " + computerSelection), console.log(computerWin = "Oh sorry! You lose!"))
+
+    //increments score and returns the value
+    if(playerWin === true){
+        playerScore++
+        return console.log(playerScore)
+    }else if(computerWin === true){
+        computerScore++
+        return console.log(computerScore)
+    }
 }

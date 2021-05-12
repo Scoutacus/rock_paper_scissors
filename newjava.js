99f23b32eac979108c5c6dce3a3780ed044ca1fe
Scoutacus <vanguardacus@gmail.com>
2021-05-12 18:27:00 -0400

    Updated index.html to reflect score results and updated newjava.js to update said results until either player reaches 5, it then resets the score to begin a new game

--- newjava.js
+++ newjava.js
@@ -15,27 +15,26 @@ function computerPlay(){
 // const scissors = function(){
 //     singleRound("scissors", computerSelection= computerPlay());
 // }
-
+let result
+let score = 0
+let cpuScore = 0
 //function that runs a single round of RPS
 function singleRound(playerSelection, computerSelection){
-    let playerWin
-    let computerWin
-
 
     //RPS logic
     if (playerSelection == "rock" && computerSelection == "scissors"){
         console.log("Computer chose : " + computerSelection);
-        console.log(playerWin = "Congrats, you win!");
+        console.log(result = "Congrats, you win!");
     } else if(playerSelection == "paper" && computerSelection == "rock"){
         console.log("Computer chose : " + computerSelection);
-        console.log(playerWin = "Congrats, you win!")
+        console.log(result = "Congrats, you win!")
     } else if(playerSelection == "scissors" && computerSelection == "paper"){
         console.log("Computer chose : " + computerSelection);
-        console.log(playerWin = "Congrats, you win!")
+        console.log(result = "Congrats, you win!")
     } else if(playerSelection === computerSelection){
         console.log("Computer chose : " + computerSelection);
-        console.log("Draw!")
-    } else(console.log("Computer chose : " + computerSelection), console.log(computerWin = "Oh sorry! You lose!"))
+        console.log(result = "Draw!")
+    } else(console.log("Computer chose : " + computerSelection), console.log(result = "Oh sorry! You lose!"))
     
 }
 
@@ -48,3 +47,29 @@ document.getElementById('paper').onclick = function(){
 document.getElementById('scissors').onclick = function(){
     singleRound("scissors", computerPlay());
 }
+document.getElementById('container').onclick = function(){
+    if(result == "Congrats, you win!"){
+        result = undefined;
+        score ++
+        document.getElementById("player").innerHTML = "Player Score: " + score;
+        console.log("Your score is: " + score);
+    }else if(result == "Oh sorry! You lose!"){
+        result = undefined;
+        cpuScore ++
+        document.getElementById("cpu").innerHTML = "CPU Score: " + cpuScore;
+        console.log("CPU score is " + cpuScore)
+    } else if(result == "Draw!"){
+        result = undefined;
+    }
+    if(score == 5){
+        alert("Hey, you actually won!");
+        score = 0;
+        cpuScore = 0;
+    } else if(cpuScore == 5){
+        alert("Aw man, I'm so sorry, but you lose!")
+        cpuScore = 0;
+        score = 0;
+    }
+    
+}
+
